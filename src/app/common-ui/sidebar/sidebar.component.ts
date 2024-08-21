4c73ada2764e324fc3741991c910c3fd638eb959
LizaAkishina <lizaakishina@gmail.com>
2024-08-21 18:23:50 +0300

    feat: сайдбар, рефреш токенов

--- src/app/common-ui/sidebar/sidebar.component.ts
+++ src/app/common-ui/sidebar/sidebar.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { AsyncPipe, JsonPipe, NgForOf } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { SubscriberCardComponent } from './subscriber-card/subscriber-card.component';
+import { ProfileService } from '../../data/services/profile.service';
+
+/*@for (profile of subscribers$ | async; track profile.id) {
+  <app-subscriber-card
+    [profile]="profile">
+  </app-subscriber-card>
+}*/
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [NgForOf, RouterLink, JsonPipe, SubscriberCardComponent, AsyncPipe],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
+  profileService = inject(ProfileService)
 
+  subscribers$ = this.profileService.getSubscribersShortList()
+
+  menuItems = [
+    {
+      label: 'Моя страница',
+      link: ''
+    },
+    {
+      label: 'Чаты',
+      link: 'chats'
+    },
+    {
+      label: 'Поиск',
+      link: 'search'
+    }
+  ]
 }
--- src/app/data/services/profile.service.ts
+++ src/app/data/services/profile.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Profile } from '../interfaces/profile.interface';
+import { Pageble } from '../interfaces/pageble.interface';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +11,19 @@ export class ProfileService {
   http:HttpClient = inject(HttpClient) //запрос у ангуляр сущность httpClient
 
   baseApiUrl = 'https://icherniakov.ru/yt-course/'
+
   getTestAccounts() {
     return this.http.get<Profile[]>(`${this.baseApiUrl}account/test_accounts`)
+  }
+
+  getSubscribersShortList() {
+    return this.http.get<Pageble<Profile[]>>(`${this.baseApiUrl}account/subscribers`)
+    .pipe(
+      map(res => res.items.slice(0, 3))
+    )
+  }
+
+  getMe() {
+    return this.http.get<Profile>(`${this.baseApiUrl}account/me`)
   }
 }
